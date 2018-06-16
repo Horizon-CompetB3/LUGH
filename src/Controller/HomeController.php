7b6c2c832693e20b35ba889f5ecde969a7d0d082
alexandresmagghe <alexandre-smagghe@outlook.com>
2018-06-16 15:53:11 +0200

    add css & js

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class HomeController extends Controller
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      * @return \Symfony\Component\HttpFoundation\Response
      */
     public function homePost()

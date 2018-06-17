6ff1082372bfabf6944366f4c630df4ac1710c0a
Alexandre Smagghe <alex.s95120@gmail.com>
2018-06-17 22:14:11 +0200

    style connection page

--- src/Controller/AddArtisteController.php
+++ src/Controller/AddArtisteController.php
@@ -17,35 +17,7 @@ class AddArtisteController extends Controller
      * @param UserPasswordEncoderInterface $passwordEncoder
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function AddArtisteAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
-    {
-// creates a artiste and gives it some dummy data for this example
-        $artiste = new Artiste();
-        $form = $this->createForm(ArtisteType::class, $artiste);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $password = $passwordEncoder->encodePassword($artiste, $artiste->getPlainPassword());
-            $artiste->setPassword($password);
-            // $form->getData() holds the submitted values
-            // but, the original `$task` variable has also been updated
-            $artiste = $form->getData();
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($artiste);
-            $entityManager->flush();
 
 
-            // ... perform some action, such as saving the task to the database
-            // for example, if Task is a Doctrine entity, save it!
-            // $entityManager = $this->getDoctrine()->getManager();
-            // $entityManager->persist($task);
-            // $entityManager->flush();
 
-            return $this->redirectToRoute('home');
-        }
-
-        return $this->render('Registration/addartiste.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
 }

d72ddf06c018f0748ace9f493b0cf4c4453b6cb7
alexandresmagghe <alexandre-smagghe@outlook.com>
2018-06-21 11:38:16 +0200

    add anim scroll

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,34 +1,4 @@
-$(function() {
-
-    var divs = $('.section-home');
-    var div = 0;
-    div = -1
-    divs.each(function(i) {
-        if (div < 0 && ($(this).offset().top >=       $(window).scrollTop())) {
-            div = i;
-        }
-    });
-    $(window).on('mousewheel DOMMouseScroll', function(e) {
-        if (e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0) {
-            if (div > 0) {
-                div--;
-            }
-        } else {
-            if (div < divs.length) {
-                div++;
-            }
-        }
-        $('html,body').stop().animate({
-            scrollTop: divs.eq(div).offset().top
-        }, 800);
-        return false;
-    });
-    $(window).resize(function() {
-        $('html,body').scrollTop(divs.eq(div).offset().top);
-        console.log(div)
-    });
+$(document).ready(function() {
+    $('#fullpage').fullpage();
 });
-
-
-
 
